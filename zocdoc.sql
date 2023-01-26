03c34126d0437f26d09d7765d88a3a4e7cb928cb
djaycat <54156037+djoyce17@users.noreply.github.com>
2023-01-26 01:06:10 -0500

    Update zocdoc.sql

--- zocdoc.sql
+++ zocdoc.sql
@@ -1,12 +1,12 @@
 --I created a cte for each column because I find this to be more readable and easy to make changes to
 
---calculating the total appointments that took place within 28 days of moving to var pricing model
+--calculating Total appointments created and took place within 28 days of when the practice moved to the variable pricing model
 WITH 
 appts AS
 (
 	SELECT 
 		practice_id
-		COUNT(*) AS appts_28_days_post_flip
+		, COUNT(*) AS appts_28_days_post_flip
 	FROM database.appointments a
 	JOIN database.practices p
 	ON a.practice_id = p.practice_id
@@ -18,20 +18,20 @@ appts AS
 )
 ,
 
---calculating the new patient appointments that took place within 28 days of moving to var price model
+--calculating New patient appointments created and took place within 28 days of when the practice moved to the variable pricing model
 new_appts AS
 (
 	SELECT 
 		practice_id
-		COUNT(*) AS appts_new_patients_28_days_post_flip
+		, COUNT(*) AS appts_new_patients_28_days_post_flip
 	FROM database.appointments a
 	JOIN database.practices p
 	ON a.practice_id = p.practice_id
 	WHERE 1=1
-	AND datediff(‘day’, p.variable_pricing_activation_time_utc, a.created_at) BETWEEN 0 AND 28
-	AND a.datediff(‘day’, p.variable_pricing_activation_time_utc, a.appointment_time_utc) BETWEEN 0 AND 28
 	AND NOT is_cancelled
 	AND is_new_patient
+	AND datediff(‘day’, p.variable_pricing_activation_time_utc, a.created_at) BETWEEN 0 AND 28
+	AND a.datediff(‘day’, p.variable_pricing_activation_time_utc, a.appointment_time_utc) BETWEEN 0 AND 28
 	GROUP BY 1
 )
 ,
@@ -41,15 +41,15 @@ canc AS
 (
 	SELECT 
 		practice_id
-		COUNT(*) AS appts_new_patient_cancelled_28_days_post_flip
+		, COUNT(*) AS appts_new_patient_cancelled_28_days_post_flip
 	FROM database.appointments a
 	JOIN database.practices p
 	ON a.practice_id = p.practice_id
-	And datediff(‘day’, p.variable_pricing_activation_time_utc, a.created_at) <= 28
-	And a.datediff(‘day’, p.variable_pricing_activation_time_utc, a.appointment_time_utc) <= 28
 	WHERE 1=1
 	AND is_cancelled
-	And is_new_patient
+	AND is_new_patient
+	AND datediff(‘day’, p.variable_pricing_activation_time_utc, a.created_at) BETWEEN 0 AND 28
+	AND a.datediff(‘day’, p.variable_pricing_activation_time_utc, a.appointment_time_utc) BETWEEN 0 AND 28
 	GROUP BY 1
 )
 ,
@@ -59,14 +59,21 @@ cap AS
 (
 	SELECT 
 		practice_id
-		COUNT(*) AS negative_spend_cap_changes_28_days
+		, COUNT(*) AS negative_spend_cap_changes_28_days
 	FROM database.spend_caps sc
 	JOIN database.practices p
 	ON sc.practice_id = p.practice_id
-	AND datediff(‘day’, p.variable_pricing_activation_time_utc, sc.start_date) <= 28
-	AND datediff(‘day’, p.variable_pricing_activation_time_utc, sc.end_date) <= 28
 	WHERE 1=1
-	AND start_cap_amount <> end_cap_amount
+	AND datediff(‘day’, p.variable_pricing_activation_time_utc, sc.start_date) BETWEEN 0 AND 28
+	AND 
+	(
+		datediff(‘day’, p.variable_pricing_activation_time_utc, sc.end_date) BETWEEN 0 AND 28
+		OR end_date IS NULL
+	)
+	AND start_cap_amount <> end_cap_amount --i was not sure if a new row was added if a customer enters the same cap amount as the previous period so i erred on the side of caution
+	
+	--make sure this captures the most recent field where the end date is null
+	
 	GROUP BY 1
 )
 ,
@@ -76,20 +83,39 @@ lock AS
 (
 	SELECT 
 		practice_id
-		COUNT(*) AS spend_locks_28_days
-	FROM database.spend_caps sc
+		, COUNT(*) AS spend_locks_28_days
+	FROM database.spend_locks sl
 	JOIN database.practices p
-	ON sc.practice_id = p.practice_id
-	AND datediff(‘day’, p.variable_pricing_activation_time_utc, sc.start_date) <= 28
-	AND datediff(‘day’, p.variable_pricing_activation_time_utc, sc.end_date) <= 28
+	ON sl.practice_id = p.practice_id
 	WHERE 1=1
-	AND start_lock_amount <> end_lock_amount
+	AND datediff(‘day’, p.variable_pricing_activation_time_utc, sl.start_date) <= 28
+	AND 
+	(
+		datediff(‘day’, p.variable_pricing_activation_time_utc, sl.end_date) <= 28
+		OR end_date IS NULL
+	)
+	--make sure this captures the most recent field where the end date is null
 	GROUP BY 1
 )
 
 SELECT 
-	practice_id,
-	date_trunc(‘day’, practice_activation_time_utc) AS practice_activation_date_utc,
-	date_trunc(‘day’, variable_pricing_activation_time_utc) AS variable_pricing_activation_date_utc,
-	date_add('day', 28, variable_pricing_activation_date_utc) AS days_28_post_flip
-FROM database.practices
+	practice_id								AS practice_id
+	, date_trunc(‘day’, practice_activation_time_utc) 			AS practice_activation_date_utc,
+	, date_trunc(‘day’, variable_pricing_activation_time_utc) 		AS variable_pricing_activation_date_utc,
+	, date_add('day', 28, variable_pricing_activation_date_utc) 		AS days_28_post_flip
+	, appts.appts_28_days_post_flip 					AS appts_28_days_post_flip
+	, new_appts.appts_new_patients_28_days_post_flip 			AS appts_new_patients_28_days_post_flip
+	, canc.appts_new_patient_cancelled_28_days_post_flip 			AS appts_new_patient_cancelled_28_days_post_flip
+	, cap.negative_spend_cap_changes_28_days				AS negative_spend_cap_changes_28_days
+	, lock.spend_locks_28_days						AS spend_locks_28_days
+FROM database.practices pr
+LEFT JOIN appts
+ON pr.practice_id = appts.practice_id
+LEFT JOIN new_appts
+ON pr.practice_id = new_appts.practice_id
+LEFT JOIN canc
+ON pr.practice_id = canc.practice_id
+LEFT JOIN cap
+ON pr.practice_id = cap.practice_id
+LEFT JOIN lock
+ON pr.practice_id = lock.practice_id
